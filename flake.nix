095a85f60397f83d9dd625d805e2ee62e9d1757d
Felix Ding <felixding1989@gmail.com>
2025-12-24 14:21:59 -0800

    adds shell hook

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
   description = "Rust project with OpenSSL and protoc";
-  
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
@@ -15,36 +13,35 @@
           rustc
           cargo
           rust-analyzer
-          
+
           # Build dependencies
           openssl
           protobuf
-          pkg-config  # Needed for OpenSSL discovery
+          pkg-config # Needed for OpenSSL discovery
         ];
-        
+
         # Environment variables for build scripts
         OPENSSL_DIR = "${pkgs.openssl.dev}";
         OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
         PROTOC = "${pkgs.protobuf}/bin/protoc";
         PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+        shellHook = ''
+          exec ${pkgs.zsh}/bin/zsh
+        '';
       };
-      
+
       packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
         pname = "my-app";
         version = "0.1.0";
         src = ./.;
         cargoLock.lockFile = ./Cargo.lock;
-        
+
         # Native build dependencies
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          protobuf
-        ];
-        
+        nativeBuildInputs = with pkgs; [ pkg-config protobuf ];
+
         # Runtime/link dependencies
-        buildInputs = with pkgs; [
-          openssl
-        ];
+        buildInputs = with pkgs; [ openssl ];
       };
     };
 }
